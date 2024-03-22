3728c93974d7bb6a5249ccc4321ae1a1e9d89ca1
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-03-22 13:07:40 +0600

    vercel.json file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
